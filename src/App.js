701d7f7959beb6705d00c9253b35c7a666964fba
Jatinder Kaur <jatinderkaurdevelop@gmail.com>
2022-09-23 21:27:38 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [second, setSecond] = useState('00');
+  const [minute, setMinute] = useState('00');
+  const [counter, setCounter] = useState(3);
+  const [isActive, setIsActive] = useState(false);
+  const [userCount, setUserCount] = useState(3)
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+        if (minuteCounter === parseInt(userCount)) {
+          setIsActive(false);
+          setCounter(0);
+          setSecond('00');
+          setMinute('00')
+        }
+        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}` : secondCounter;
+        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+        setCounter(counter => counter + 1);
+
+      }, 10)
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter])
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setCounter(0);
+    setSecond('00');
+    setMinute('00')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex'>
+      <input type="number" placeholder="Enter Mins" value={userCount} onChange={(e) => setUserCount(e.target.value)} />
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? "Pause" : "Start"}</button>
+      <button onClick={resetTimer}>Reset</button>
+      <div className="time">
+        <span className="minute">{minute}</span>
+        <span>:</span>
+        <span className="second">{second}</span>
+      </div>
     </div>
+
   );
 }
 
